19a713a2cb52e1ed906d9a0285c0507e0dd72ef8
Mstislav <mstislav@idpowers.com>
2016-09-27 16:00:32 +0500

    working with user-ids add put method

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, abort
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -6,8 +6,18 @@ api = Api(app)
 
 
 class Offer(Resource):
+    def __init__(self):
+        self.id = request.headers.get('user-id')
+        if not self.id:
+            abort(403)
+
     def get(self):
-        return {'hello': 'world'}
+        id = request.headers.get('user-id')
+        return {'get': id}
+
+    def put(self):
+        id = request.headers.get('user-id')
+        return {'put': id}
 
 api.add_resource(Offer, '/offer/')
 
